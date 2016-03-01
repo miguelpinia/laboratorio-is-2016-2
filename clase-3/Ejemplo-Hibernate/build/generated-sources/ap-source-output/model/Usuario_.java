31b8ddac2c76fbd8f78104579ff3738629d4a8b4
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-03-01 13:57:34 -0600

    Agrega el ejemplo de Hibernate.
    Agrega el ejemplo utilizado durante la clase mostrando la creación de
    clases que represetan el mapeo de relaciones.
    
    Esto es realizado a través de Hibernate.

---
+++ clase-3/Ejemplo-Hibernate/build/generated-sources/ap-source-output/model/Usuario_.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-02-23T16:42:22")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> apellidos;
+    public static volatile SingularAttribute<Usuario, Date> fechaRegistro;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+    public static volatile SingularAttribute<Usuario, String> nombres;
+
+}
