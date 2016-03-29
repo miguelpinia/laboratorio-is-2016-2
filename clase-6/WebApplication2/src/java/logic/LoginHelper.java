0b2eb18c22d966862db847b98a22cf64761863d3
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-03-28 22:14:29 -0600

    Agrega una clase de ayuda.
    La clase permite realizar consultas y transacciones con la base de datos.

---
+++ clase-6/WebApplication2/src/java/logic/LoginHelper.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import model.Login;
+
+/**
+ *
+ * @author miguel
+ */
+public class LoginHelper {
+
+    private Session session;
+
+    public LoginHelper() {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+
+    public Login getLoginPorNombre(String nombre) {
+        Login usuario;
+        try {
+            Transaction tx = session.beginTransaction();
+            Query q = session.getNamedQuery("BuscaPorNombre").setString("usuario", nombre);
+            return (Login) q.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
