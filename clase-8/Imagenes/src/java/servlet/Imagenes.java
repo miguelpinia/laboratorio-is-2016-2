a483ae2507154ae14f1140b026301ef56835bbdb
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2016-05-16 22:46:16 -0500

    Agrega servlet para despachar las imágenes.
    Déjamos que sea un servlet vía el mapping de web.xml el que se encargue
    de despachar las imagenes solicitadas a la aplicación.

---
+++ clase-8/Imagenes/src/java/servlet/Imagenes.java
@@ -0,0 +1,62 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Logic.ImageHelper;
+import model.Imagen;
+
+/**
+ *
+ * @author miguel
+ */
+public class Imagenes extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     *
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String uri = request.getRequestURI();
+        System.out.println(uri);
+        uri = uri.replace(request.getContextPath() + "/", "");
+        uri = uri.replace("%20", " ");
+        System.out.println(uri);
+        uri = uri.substring(uri.indexOf("/") + 1);
+        System.out.println("Uri: " + uri);
+        ImageHelper helper = new ImageHelper();
+        Imagen img = helper.getImagenPorNombre(uri);
+        response.setContentType(img.getMimeType().getNombre());
+        ServletOutputStream out = response.getOutputStream();
+        out.write(img.getContenido());
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet get method. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     *
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }// </editor-fold>
+
+}
